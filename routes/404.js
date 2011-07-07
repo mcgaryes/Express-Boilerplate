e5f6db37f1a8102c5c7af9a6ed56ec00824164ec
Eric McGary <mcgaryes@gmail.com>
2011-07-07 13:22:53 -0400

    Included 404.css and 404.js for consistency

--- routes/404.js
+++ routes/404.js
@@ -2,7 +2,7 @@ module.exports = function(app) {
    
     // http response
     app.get('*', function(req, res, next) {
-        res.render( '404.ejs' , { title:"404" });
+        res.render( '404.ejs' , { title:"404",location:'404' });
     });
    
 }; 
